f4a13076e0d8447371fe18504522a54906666847
sebatustra <sebatustra@protonmail.com>
2024-01-22 21:59:49 -0300

    ready to push

--- components/StudentIntroList.tsx
+++ components/StudentIntroList.tsx
@@ -2,32 +2,53 @@ import { Card } from './Card'
 import { FC, useEffect, useState } from 'react'
 import { StudentIntro } from '../models/StudentIntro'
 import * as web3 from '@solana/web3.js'
-
-const STUDENT_INTRO_PROGRAM_ID = 'HdE95RSVsdb315jfJtaykXhXY478h53X6okDupVfY9yf'
+import { IntroCoordinator } from '../coordinators/IntroCoordinator'
+import { Center, HStack, Button, Spacer, Input } from '@chakra-ui/react'
 
 export const StudentIntroList: FC = () => {
-    const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
+    const connection = new web3.Connection("https://devnet.helius-rpc.com/?api-key=290edc0c-f52a-4681-90d2-8accac0e5e2b")
     const [studentIntros, setStudentIntros] = useState<StudentIntro[]>([])
+    const [page, setPage] = useState<number>(1);
+    const [search, setSearch] = useState<string>("");
 
     useEffect(() => {
-        connection.getProgramAccounts(new web3.PublicKey(STUDENT_INTRO_PROGRAM_ID)).then(async (accounts) => {
-            const studentIntros: StudentIntro[] = accounts.reduce((accum: StudentIntro[], { pubkey, account }) => {
-                const studentIntro = StudentIntro.deserialize(account.data)
-                if (!studentIntro) {
-                    return accum
-                }
-
-                return [...accum, studentIntro]
-            }, [])
-            setStudentIntros(studentIntros)
-        })
-    }, [])
+        IntroCoordinator.fetchPage(
+            connection,
+            page,
+            5,
+            search,
+            search !== ""
+        ).then(setStudentIntros)
+    }, [page, search])
     
     return (
         <div>
+            <Center>
+                <Input
+                    id='search'
+                    color='gray.400'
+                    onChange={event => setSearch(event.currentTarget.value)}
+                    placeholder='Search'
+                    w='97%'
+                    mt={2}
+                    mb={2}
+                />
+            </Center>
             {
                 studentIntros.map((studentIntro, i) => <Card key={i} studentIntro={studentIntro} /> )
             }
+            <Center>
+                <HStack w='full' mt={2} mb={8} ml={4} mr={4}>
+                    {
+                    page > 1 && <Button onClick={() => setPage(page - 1)}>Previous</Button>
+                    }
+                    <Spacer />
+                    {
+                    IntroCoordinator.accounts.length > page * 5 &&
+                        <Button onClick={() => setPage(page + 1)}>Next</Button>
+                    }
+                </HStack>
+            </Center>
         </div>
     )
 }
